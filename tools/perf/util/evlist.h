37579355725263374fbfd119db4f9d6610dd6b9d
Wang Nan <wangnan0@huawei.com>
2016-01-26 07:17:59 +0000

    perf tools: Add API to pause a channel
    perf_evlist__channel_toggle_paused() is introduced to pause/resume a
    channel in an evlist. Utilize PERF_EVENT_IOC_PAUSE_OUTPUT ioctl.
    Following commits use perf_evlist__channel_toggle_paused() to ensure
    overwrite ring buffer is turned off before reading.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Signed-off-by: He Kuang <hekuang@huawei.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com

--- tools/perf/util/evlist.h
+++ tools/perf/util/evlist.h
@@ -180,6 +180,8 @@ perf_evlist__get_mmap(struct perf_evlist *evlist,
 	return &evlist->mmap[idx];
 }
 
+int perf_evlist__channel_toggle_paused(struct perf_evlist *evlist,
+				       int channel, bool pause);
 int perf_evlist__open(struct perf_evlist *evlist);
 void perf_evlist__close(struct perf_evlist *evlist);
 
