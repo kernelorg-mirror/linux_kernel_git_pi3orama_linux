f1a479740aa55b1246500501df99861bcfbc1790
Wang Nan <wangnan0@huawei.com>
2016-11-14 11:16:26 +0800

    perf clang jit: Allow jitted perf hook access BPF maps
    Newly introduced jit-helpers.[ch] defines a series of helpers which helps
    jitted perf hook functions accessing BPF maps defined in their BPF scripts.
    The helpers fetches fd of 'struct bpf_map' from 'struct bpf_object' and the
    address of 'struct bpf_map_def' in jitted file. 'struct bpf_object' is the
    context passed to hooks.
    
     Example:
    
      $ cat ./test.c
      /*******************************************************/
      #define SEC(name) __attribute__((section(name), used))
      #define BPF_MAP_TYPE_ARRAY 2
      #define BPF_MAP_TYPE_PERF_EVENT_ARRAY 4
      #define BPF_FUNC_map_lookup_elem 1
      static void *(*bpf_map_lookup_elem)(void *map, void *key) =
          (void *) BPF_FUNC_map_lookup_elem;
      struct bpf_map_def {
          unsigned int type;
          unsigned int key_size;
          unsigned int value_size;
          unsigned int max_entries;
      };
      struct bpf_map_def SEC("maps") counter = {
          .type = BPF_MAP_TYPE_ARRAY,
          .key_size = sizeof(int),
          .value_size = sizeof(int),
          .max_entries = 1,
      };
      extern int jit_helper__map_update_elem(void *ctx, struct bpf_map_def *map,
                             void *key, void *value, unsigned long flags);
      extern int jit_helper__map_lookup_elem(void *ctx, struct bpf_map_def *map,
                             void *key, void *value);
      SEC("sys_close=SyS_close")
      int sys_close(void *ctx)
      {
          int key = 0;
          int *value;
          value = bpf_map_lookup_elem(&counter, &key);
          if (!value)
              return 0;
          __sync_fetch_and_add(value, 1);
          return 0;
      }
      extern int printf(const char *fmt, ...);
      SEC("perfhook:record_start")
      void record_start(void *ctx)
      {
          int key = 0;
          int value = 100000000;
          printf("Welcom to perf record\n");
          jit_helper__map_update_elem(ctx, &counter, &key, &value, 0);
      }
    
      SEC("perfhook:record_end")
      void record_end(void *ctx)
      {
          int key = 0;
          int value;
          jit_helper__map_lookup_elem(ctx, &counter, &key, &value);
          printf("Goodbye, perf record, value=%d\n", value);
      }
      char _license[] SEC("license") = "GPL";
      int _version SEC("version") = LINUX_VERSION_CODE;
      /*******************************************************/
      $ sudo perf record  -e ./test.c echo Hehe
      Welcom to perf record
      Hehe
      [ perf record: Woken up 1 times to write data ]
      Goodbye, perf record, value=100000644
      [ perf record: Captured and wrote 0.014 MB perf.data ]
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexei Starovoitov <ast@fb.com>
    Cc: He Kuang <hekuang@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com

---
+++ tools/perf/util/jit-helpers.h
@@ -0,0 +1,30 @@
+#ifndef JIT_HELPERS_H
+#define JIT_HELPERS_H
+
+#include <stdint.h>
+#include <util/perf-hooks.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define JIT_HELPER_FUNC_NAME(name) jit_helper__##name
+
+#define JIT_HELPER(type, name, ...) \
+type JIT_HELPER_FUNC_NAME(name)(__VA_ARGS__)
+
+#define JIT_BPF_MAP_HELPER(name, ...) \
+	JIT_HELPER(int, map_##name, void *ctx, void *map, ##__VA_ARGS__)
+
+extern JIT_BPF_MAP_HELPER(update_elem, void *key, void *value, uint64_t flags);
+extern JIT_BPF_MAP_HELPER(lookup_elem, void *key, void *value);
+extern JIT_BPF_MAP_HELPER(get_next_key, void *key, void *next_key);
+extern JIT_BPF_MAP_HELPER(pin, const char *pathname);
+
+extern JIT_HELPER(int, map_get, const char *pathname);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
